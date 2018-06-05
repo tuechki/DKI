23092e130e8a590dd0807320912f0b21d4d5ae7e
tuechki <dtuechki@gmail.com>
2018-06-05 19:18:10 +0300

    Update selection_sort.cc

--- selection_sort.cc
+++ selection_sort.cc
@@ -6,7 +6,7 @@ int main()
 {
     int array[5] = {32,45,10,7,18};
     
-    for(int counter = 0, sorted_array_counter = 0; counter < 5; counter++){
+    for(int counter = 0; counter < 5; counter++){
         
         for(int index = counter + 1; index < 5; index++){
             if(array[counter] > array[index]){
